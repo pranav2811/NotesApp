53b123bf1c3ccbba7680b061ce8fb88d649989e8
Pranav <powar.pranav29@gmail.com>
2023-01-08 22:35:20 +0530

    created view model

--- app/src/main/java/com/pranav/notes/MainActivity.java
+++ app/src/main/java/com/pranav/notes/MainActivity.java
@@ -1,14 +1,32 @@
 package com.pranav.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    //we need to show the view model as a reference in the main activity
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new NoteViewModelProvider.AndroidViewModelFactory(getApplication())
+                .create(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //this method will observe the changes
+                //update recycler view
+            }
+        });//the live data method that will observe changes in the db
     }
 }
